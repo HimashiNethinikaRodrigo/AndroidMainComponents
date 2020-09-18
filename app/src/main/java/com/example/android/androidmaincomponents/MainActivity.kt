be92fab00f702306477a0c88c3c88d96507f5eca
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-09-18 15:10:43 +0200

    add onclick listener to count up button to increase the value of dice by 1 when click it

--- app/src/main/java/com/example/android/androidmaincomponents/MainActivity.kt
+++ app/src/main/java/com/example/android/androidmaincomponents/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.android.androidmaincomponents
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,12 +9,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         buttonRoll.setOnClickListener { rollDice() }
+        buttonCountUp.setOnClickListener { countUp() }
     }
 
     private fun rollDice() {
-        Toast.makeText(this, "Roll Button Clicked", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(this, "Roll Button Clicked", Toast.LENGTH_SHORT).show()
         textViewInfo.text = "Dice Rolled!"
         val randomInt = (1..6).random()
         textViewDice.text = randomInt.toString()
     }
+
+    private fun countUp() {
+        var text = textViewDice.text.toString().toIntOrNull()
+        if (text != null) {
+            if (text < 6)
+                textViewDice.text = (++text).toString()
+        } else
+            textViewDice.text = "1"
+    }
+
 }
