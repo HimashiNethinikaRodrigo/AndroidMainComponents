75541b3828cca3ff7a15149f5657770653a33263
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-09-18 14:25:07 +0200

    add on click listener to the roll button to show up toast when button clicked

--- app/src/main/java/com/example/android/androidmaincomponents/MainActivity.kt
+++ app/src/main/java/com/example/android/androidmaincomponents/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.android.androidmaincomponents
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonRoll.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        Toast.makeText(this, "Roll Button Clicked", Toast.LENGTH_SHORT).show()
     }
 }
